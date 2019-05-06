3daf5f9ae3b76246babf591196cc3e46e7e9a289
Chisnits <chasederr@gmail.com>
2019-05-05 21:59:30 -0600

    abstractions

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // Constructor Function
 
 function Circle(radius) {
-    // console.log('this', this);
-    this.radius = radius,
+    // let color = 'red'; // local variable inside of this function
+
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 };
+
+    let computerOptiumumLocation = function(factor) {
+        // ...
+    }
+
     this.draw = function() {
+        //scope is temperary but the closure remains.
+        computerOptiumumLocation(0.1);
+        //defaultLocation
+
+        // this.radius
+
         console.log('draw');
     }
 }
 
-const circle = new Circle(1);
+const circle = new Circle(10);
 
 circle.location = {x : 1};
 circle['location'] = {x: 1}; // the same as dot notation above.
@@ -21,9 +35,14 @@ for (let key in circle) {
     if(typeof circle[key] !== 'function')
     console.log(key, circle[key]);
 }
+// to enumerate all of the members of an object use for in loop
 
 const keys = Object.keys(circle);
 console.log(keys);
 
+// to get all of the keys in an object use Object.keys
+
 if ('radius' in circle)
     console.log('Circle has a radius');
+
+// to check for the existance of a property or a method in an object use the in operator.
