95c9784d375e8804c7f35c231d71e5e4d7036980
Chisnits <chasederr@gmail.com>
2019-05-27 22:48:29 -0600

    completed stopwatch challenge

--- index.js
+++ index.js
@@ -46,3 +46,39 @@ if ('radius' in circle)
     console.log('Circle has a radius');
 
 // to check for the existance of a property or a method in an object use the in operator.
+
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function() {
+        if (running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true;
+        startTime = new Date();
+    }
+
+    this.stop = function() {
+        if (!running)
+        throw new Error('Stopwatch is not started');
+
+        running = false;
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    }
+
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    }
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            return duration;
+        }
+    });
+}
